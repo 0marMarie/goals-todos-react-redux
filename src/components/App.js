e23a6caa51293accc252392fd0fd83f6bd30237d
Omar Marie <omarhamed440@gmail.com>
2021-10-07 20:36:04 +0200

    Refactoring Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
+import React from "react"
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
 
-function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    const { dispatch } = this.props
+
+    dispatch(handleInitialData())
+  }
+
+  render() {
+    if (this.props.loading) {
+      return <h3>Loading !!</h3>
+    }
+    return (
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
